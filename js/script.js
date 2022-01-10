c7a738567af0a4b69cfd7759d1adb2cf3ce1ec43
AntonioFBove <antoniofbove@gmail.com>
2022-01-10 17:52:36 +0100

    bonus

--- js/script.js
+++ js/script.js
@@ -1,3 +1,33 @@
+function addMember (newMember, container) {
+  // creiamo la carta
+  const card = document.createElement('div');
+  card.className = 'team-card';
+  container.append(card);
+  
+  // inseriamo l'immagine
+  const cardImage = document.createElement('div');
+  cardImage.className = 'card-image';
+  card.append(cardImage);
+  
+  const image = document.createElement('img');
+  image.src = `img/${newMember.image}`;
+  image.alt = newMember.name;
+  cardImage.append(image);
+  
+  // inseriamo il testo
+  const cardText = document.createElement('div');
+  cardText.className = 'card-text';
+  card.append(cardText);
+  
+  const tmName = document.createElement('h3');
+  tmName.append(newMember.name);
+  cardText.append(tmName);
+  
+  const tmRole = document.createElement('p');
+  tmRole.append(newMember.role);
+  cardText.append(tmRole);
+  };
+
 const team = 
   [
     {
@@ -43,38 +73,12 @@ for (let i = 0; i < team.length; i++) {
 const formButtonHtml = document.getElementById('addMemberButton');
 
 formButtonHtml.addEventListener('click', function () {
-  addMember();
+  const formInputHtml = document.querySelectorAll('.form-container > input');  
+  const newMember = {};
+
+  formInputHtml.forEach(element => {
+    newMember[element.id] = element.value;
+  });
+  
+  addMember(newMember, teamContainerHtml);
 })
-
-
-
-
-function addMember (newMember, container) {
-// creiamo la carta
-const card = document.createElement('div');
-card.className = 'team-card';
-container.append(card);
-
-// inseriamo l'immagine
-const cardImage = document.createElement('div');
-cardImage.className = 'card-image';
-card.append(cardImage);
-
-const image = document.createElement('img');
-image.src = `img/${newMember.image}`;
-image.alt = newMember.name;
-cardImage.append(image);
-
-// inseriamo il testo
-const cardText = document.createElement('div');
-cardText.className = 'card-text';
-card.append(cardText);
-
-const tmName = document.createElement('h3');
-tmName.append(newMember.name);
-cardText.append(tmName);
-
-const tmRole = document.createElement('p');
-tmRole.append(newMember.role);
-cardText.append(tmRole);
-};
