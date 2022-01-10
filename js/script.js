604c568b8697e26eba786d76fc3045586ab009d9
AntonioFBove <antoniofbove@gmail.com>
2022-01-10 17:35:01 +0100

    aggiunta funzione addMember()

--- js/script.js
+++ js/script.js
@@ -37,44 +37,44 @@ const teamContainerHtml = document.querySelector('.team-container')
 
 for (let i = 0; i < team.length; i++) {
   const currentTeamMember = team[i];
-
-  // creiamo la carta
-  const card = document.createElement('div');
-  card.className = 'team-card';
-  teamContainerHtml.append(card);
-  
-  // inseriamo l'immagine
-  const cardImage = document.createElement('div');
-  cardImage.className = 'card-image';
-  card.append(cardImage);
-
-  const image = document.createElement('img');
-  image.src = `img/${currentTeamMember.image}`;
-  image.alt = currentTeamMember.name;
-  cardImage.append(image);
-  
-  // inseriamo il testo
-  const cardText = document.createElement('div');
-  cardText.className = 'card-text';
-  card.append(cardText);
-
-  const tmName = document.createElement('h3');
-  tmName.append(currentTeamMember.name);
-  cardText.append(tmName);
-
-  const tmRole = document.createElement('p');
-  tmRole.append(currentTeamMember.role);
-  cardText.append(tmRole);
-
-
-  
-  
-  // const teamImage = document.createElement('div');
-  // teamImage.className = 'card-image';
-  // teamImage.src = currentTeamMember.image;
-  // teamImage.alt = currentTeamMember.name;
-  // card.append(teamImage);
-
-
-
+  addMember(currentTeamMember, teamContainerHtml);
 }
+
+const formButtonHtml = document.getElementById('addMemberButton');
+
+formButtonHtml.addEventListener('click', function () {
+  addMember();
+})
+
+
+
+
+function addMember (newMember, container) {
+// creiamo la carta
+const card = document.createElement('div');
+card.className = 'team-card';
+container.append(card);
+
+// inseriamo l'immagine
+const cardImage = document.createElement('div');
+cardImage.className = 'card-image';
+card.append(cardImage);
+
+const image = document.createElement('img');
+image.src = `img/${newMember.image}`;
+image.alt = newMember.name;
+cardImage.append(image);
+
+// inseriamo il testo
+const cardText = document.createElement('div');
+cardText.className = 'card-text';
+card.append(cardText);
+
+const tmName = document.createElement('h3');
+tmName.append(newMember.name);
+cardText.append(tmName);
+
+const tmRole = document.createElement('p');
+tmRole.append(newMember.role);
+cardText.append(tmRole);
+};
